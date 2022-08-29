32ea732a5308af909ee88e355ae4a6e7d01b1eb4
Aleksandr <uorkansk@yandex.ru>
2022-08-29 16:27:00 +0700

    условия если, иначе

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Your Name...");
+string nameUser = Console.ReadLine();
+if (nameUser.ToLower() == "маша")
+{
+    Console.Write("Приветствую тебя моя дорогая!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(nameUser);
+}
