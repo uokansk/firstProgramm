b1a06b6c9614185772939a0be092b3a1282ce69d
Aleksandr <uorkansk@yandex.ru>
2022-08-29 15:29:13 +0700

    деление вещественных чисел

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// Console.Write("введите первое число ");
+double firstNumber = 12;
+// Console.Write("введите второе число ");
+double secondNumber = 5;
+Console.WriteLine(firstNumber / secondNumber);
