c57336695c99ff70312765d81ada825dcfffa06f
Aleksandr <uorkansk@yandex.ru>
2022-09-01 22:48:12 +0700

    задача 11

---
+++ Exemple011_array/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)   // метод заполняет массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void Printaray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создай массив в котором 10 элементов
+
+FillArray(array);
+Printaray(array);
+Console.WriteLine();
+
+int pos = IndexOff(array, 4);
+Console.WriteLine(pos);
