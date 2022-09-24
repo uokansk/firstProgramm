def1252e9958bd7894194efcb5bd8f8fe85eed7b
Aleksandr <uorkansk@yandex.ru>
2022-09-24 19:42:36 +0700

    ввел строки с коментариями в задаче

--- Example003_sumNumbers/Program.cs
+++ Example003_sumNumbers/Program.cs
@@ -1,7 +1,13 @@
-﻿// Console.Write("введите первое число ");
-int firstNumber = new Random().Next(1, 10);
+﻿int numberA = 1;
+int numberB = 10;
+
+// Console.Write("введите первое число ");
+int firstNumber = new Random().Next(numberA, numberB);
+Console.Write("Первое число: "); 
 Console.WriteLine(firstNumber); 
 // Console.Write("введите второе число ");
-int secondNumber = new Random().Next(1, 10);
-Console.WriteLine(secondNumber); 
+int secondNumber = new Random().Next(numberA, numberB);
+Console.Write("Второе число: "); 
+Console.WriteLine(secondNumber);
+Console.Write("Сумма чисел: ");  
 Console.WriteLine(firstNumber + secondNumber);
